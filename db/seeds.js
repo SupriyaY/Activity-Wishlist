fe33499382ef9832f8a6f36f00fcb5f3a82dc138
Supriya <supriya.yerramilli@gmail.com>
2018-01-11 09:43:46 -0500

    formatting added new seed

--- db/seeds.js
+++ db/seeds.js
@@ -7,30 +7,28 @@ const mongoose = require('mongoose')
 //Connecting to moongoose database
 mongoose.connect(process.env.MONGODB_URI)
 
-mongoose.connection.once('open', () => {
-    console.log(`Mongoose has connected to MongoDB`)
-})
+mongoose
+    .connection
+    .once('open', () => {
+        console.log(`Mongoose has connected to MongoDB`)
+    })
 
-mongoose.connection.on('error', (error) => {
-    console.error(`MongoDB connection error!
-${error}
-`)
-    process.exit(-1)
-})
+mongoose
+    .connection
+    .on('error', (error) => {
+        console.error(`MongoDB connection error! ${error}`)
+        process.exit(-1)
+    })
 
 //Delete a user and add a test user
-User.remove({}).then(() => {
+User
+    .remove({})
+    .then(() => {
         const Supsy = new User({
-            username: 'SupsY',
-            name: 'Supriya Yerramilli',
-            photo_url : 'https://i.imgur.com/3XCJ5EZ.png?3'
+            username: 'SupsY', name: 'Supriya Yerramilli', 
+            photo_url: 'https://i.imgur.com/3XCJ5EZ.png?3'})
 
-        })
-
-        const miami = new City({
-            city_name: 'Miami'
-
-        })
+        const miami = new City({city_name: 'Miami'})
         const shoreClub = new Activity({
             activity_place: "Shore Club South Beach",
             activity_name: "Brunch",
@@ -40,53 +38,11 @@ User.remove({}).then(() => {
             recommend_to_friends: "yes"
         })
 
-        miami.activitiesToComplete.push(shoreClub)
-
-
-
-        const atlanta = new City({
-            city_name: 'Atlanta'
-        })
-
-        const footballGame = new Activity({
-            activity_place: 'Atlanta Falcons game',
-            activity_name: 'Football game',
-            activity_address: '1414 Andrew Young International Blvd NW, Atlanta, GA 30313',
-            activity_description: 'Football, beer, and hotdogs at Mercedes-Benz Stadium!',
-            visited: "yes",
-            recommend_to_friends: "yes"
-        })
-
-        atlanta.activitiesToComplete.push(footballGame)
-
-        Supsy.cities.push(miami, atlanta)
-
-        return Supsy.save()
-
-
-User
-    .remove({})
-    .then(() => {
-const carrie = new User({
-    username: 'CarrieB', name: 'Carrie Bradshaw', 
-    photo_url: 'https//i.imgur.com/DfCAISL.png?1'
-})
-
-        const maine = new City({city_name: 'Portland'})
-        const graffiti = new Activity({
-            activity_place: "Graffiti Walls",
-            activity_name: "Being hipster",
-            activity_address: "567 Walrus Road, Portland, Maine",
-            activity_description: "Taking pictures for IG.",
-            visited: "yes",
-            recommend_to_friends: "yes"
-        })
-
         miami
             .activitiesToComplete
-            .push(maine)
+            .push(shoreClub)
 
-        const = new City({city_name: 'Atlanta'})
+        const atlanta = new City({city_name: 'Atlanta'})
 
         const footballGame = new Activity({
             activity_place: 'Atlanta Falcons game',
@@ -97,7 +53,7 @@ const carrie = new User({
             recommend_to_friends: "yes"
         })
 
-        new york
+        atlanta
             .activitiesToComplete
             .push(footballGame)
 
@@ -107,71 +63,96 @@ const carrie = new User({
 
         return Supsy.save()
 
+    })
+    // .then(() => {     return User.create({username: 'CarrieB', name: 'Carrie
+    // Bradshaw', photo_url: 'https//i.imgur.com/DfCAISL.png?1'}) })
+    .then(() => {
+        const carrie = new User({username: 'CarrieB', 
+        name: 'Carrie Bradshaw', 
+        photo_url : 'https://i.imgur.com/DfCAISL.png?1'})
 
-    }).then(() => {
-        return User.create({
-            username: 'NintiC',
-            name: 'Ninti Chance',
-            photo_url : 'https://i.imgur.com/IcdNK6r.png?1'
-
-        })
-    }).then((ninti) => {
-            const newYork = new City({
-                city_name: 'New York'
-            })
-
-            const losAngeles = new City ({
-                city_name: 'Los Angeles'
-            })
-
-            ninti.cities.push(newYork, losAngeles)
-
-return ninti.save()
+        const portland = new City({city_name: 'Portland'})
+        const graffiti = new Activity({
+            activity_place: "Graffiti Walls",
+            activity_name: "Being hipster",
+            activity_address: "567 Walrus Road, Portland, Maine",
+            activity_description: "Taking pictures for IG.",
+            visited: "yes",
+            recommend_to_friends: "yes"
         })
 
+        portland
+            .activitiesToComplete
+            .push(graffiti)
 
-.then(() => {
-return User.create({username: 'AlexM', 
-name: 'Alex Mink', 
-photo_url : 'https://i.imgur.com/cQigItX.png?1'
-})
-})
-.then((alex) => {
+        const sanFran = new City({city_name: 'San Francisco'})
 
+        const napaValley = new Activity({
+            activity_place: 'Napa Valley',
+            activity_name: 'Wine tasting',
+            activity_address: '123 Napa Valley, San Franciso 45678',
+            activity_description: 'An afternoon of wine tasting and a tour of Napa Valley',
+            visited: "yes",
+            recommend_to_friends: "yes"
+        })
 
-const austin = new City({city_name: 'Austin'})
+        sanFran.activitiesToComplete.push(napaValley)
 
-const chicago = new City({city_name: 'Chicago'})
+        carrie.cities.push(portland, sanFran)
 
-alex.cities.push(austin, chicago)
+        return carrie.save()
+    })
+    .then(() => {
+        return User.create({username: 'NintiC', name: 'Ninti Chance', photo_url: 'https://i.imgur.com/IcdNK6r.png?1'})
+    })
+    .then((ninti) => {
+        const newYork = new City({city_name: 'New York'})
 
-return alex.save()
-})
+        const losAngeles = new City({city_name: 'Los Angeles'})
 
+        ninti
+            .cities
+            .push(newYork, losAngeles)
 
-.then(() => {
-    return User.create({username: 'ClimberBoy', 
-    name: 'Jon Ruben', 
-    photo_url : 'https://i.imgur.com/1yHRoDB.png?1'
-})
-}).then((jon) => {
+        return ninti.save()
+    })
+    .then(() => {
+        return User.create({username: 'AlexM', name: 'Alex Mink', photo_url: 'https://i.imgur.com/cQigItX.png?1'})
+    })
+    .then((alex) => {
 
-    const bolder = new City({city_name: 'Bolder'})
+        const austin = new City({city_name: 'Austin'})
 
+        const chicago = new City({city_name: 'Chicago'})
 
-    jon.cities.push(bolder)
+        alex
+            .cities
+            .push(austin, chicago)
 
-    return jon.save()
-})
+        return alex.save()
+    })
+    .then(() => {
+        return User.create({username: 'ClimberBoy', name: 'Jon Ruben', photo_url: 'https://i.imgur.com/1yHRoDB.png?1'})
+    })
+    .then((jon) => {
 
+        const bolder = new City({city_name: 'Bolder'})
 
+        jon
+            .cities
+            .push(bolder)
 
-                    .catch((error) => {
-                        console.log('Error saving seeded data!')
-                        console.log(error)
-                    }).then(() => {
-                        mongoose.connection.close()
-                        console.log(`Finished seeding database...
+        return jon.save()
+    })
+    .catch((error) => {
+        console.log('Error saving seeded data!')
+        console.log(error)
+    })
+    .then(() => {
+        mongoose
+            .connection
+            .close()
+        console.log(`Finished seeding database...
     Disconnected from MongoDB
     `)
-                    })
+    })
